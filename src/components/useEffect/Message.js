87d295e46123aabc38d3f99c02c9d093da1b5b04
Carlos Espejel <carlosespejel71@gmail.com>
2022-02-11 14:41:18 -0600

    useEffect unmount - cleanup

--- src/components/useEffect/Message.js
+++ src/components/useEffect/Message.js
@@ -1,14 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coords, setCoords] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const { x, y } = coords;
+
   useEffect(() => {
-    console.log("Componente montado");
-    return () => console.log("Componente desmontado");
+    const mouseMove = (e) => {
+      const coords = { x: e.x, y: e.y };
+      setCoords(coords);
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+      console.log("Componente desmontado");
+    };
   }, []);
 
   return (
     <div>
       <h3>Eres genial!</h3>
+      <p>
+        x: {x}, y: {y}
+      </p>
     </div>
   );
 };
