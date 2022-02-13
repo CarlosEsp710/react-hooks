8b2c10fa6a42bebea1df7ea2e202fe27e625b2dd
Carlos Espejel <carlosespejel71@gmail.com>
2022-02-13 12:04:58 -0600

    reducer example

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-//import { HookApp } from "./HookApp";
-//import { CounterApp } from "./components/useState/CounterApp";
-//import { CounterWithCustomHook } from "./components/useState/CounterWithCustomHook";
-//import { SimpleForm } from "./components/useEffect/SimpleForm";
-//import { FormWithCustomHook } from "./components/useEffect/FormWithCustomHook";
-//import { MultipleCustomHooks } from "./components/examples/MultipleCustomHooks";
-//import { FocusScreen } from "./components/useRef/FocusScreen";
-//import { RealExampleRef } from "./components/useRef/RealExampleRef";
-//import { LayoutEffect } from "./components/useLayoutEffect/LayoutEffect";
-//import { Memorize } from "./components/memos/Memorize";
-//import { MemoHook } from "./components/memos/MemoHook";
-//import { CallbackHook } from "./components/memos/CallbackHook";
-import { Padre } from "./components/homework/Padre";
+// //import { HookApp } from "./HookApp";
+// //import { CounterApp } from "./components/useState/CounterApp";
+// //import { CounterWithCustomHook } from "./components/useState/CounterWithCustomHook";
+// //import { SimpleForm } from "./components/useEffect/SimpleForm";
+// //import { FormWithCustomHook } from "./components/useEffect/FormWithCustomHook";
+// //import { MultipleCustomHooks } from "./components/examples/MultipleCustomHooks";
+// //import { FocusScreen } from "./components/useRef/FocusScreen";
+// //import { RealExampleRef } from "./components/useRef/RealExampleRef";
+// //import { LayoutEffect } from "./components/useLayoutEffect/LayoutEffect";
+// //import { Memorize } from "./components/memos/Memorize";
+// //import { MemoHook } from "./components/memos/MemoHook";
+// //import { CallbackHook } from "./components/memos/CallbackHook";
+// import { Padre } from "./components/homework/Padre";
 
-ReactDOM.render(<Padre />, document.getElementById("root"));
+// ReactDOM.render(<Padre />, document.getElementById("root"));
+
+import "./components/useReducer/intro-reducer";
